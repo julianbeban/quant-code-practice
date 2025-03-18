4b771e64a1d012c3adab954b4c53ecb57c066711
julianbeban <136780022+julianbeban@users.noreply.github.com>
2025-03-18 12:00:06 -0400

    Update monte-carlo.py

--- code/monte-carlo.py
+++ code/monte-carlo.py
@@ -1,2 +1,19 @@
 # Implementation of monte-carlo simulation to project potential security price
 
+import numpy as np
+
+for i in range(0, 10000):
+  shots_made = []
+  for k in range(0, 100):
+    r = np.random.random()
+    if(r <= .7):
+        shots_made.append(1)
+    else:
+        shots_made.append(0)
+
+  if(sum(shots_made) >= 70):
+    aggregate_shots_made.append(1)
+  else:
+    aggregate_shots_made.append(0)
+
+print(np.average(aggregate_shots_made))
